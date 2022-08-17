c877ac51e01b4569e8574985bc04ced12e7f5628
Ramiro <ramifazio@gmail.com>
2022-08-17 12:20:23 -0300

    Sugerencias D3!

--- js/index.js
+++ js/index.js
@@ -1,58 +1,57 @@
-alert('Bienvenidos a pampaTicket!');
+alert("Bienvenidos a pampaTicket!");
 
-class Ticket{
-    constructor(id, nombre, precio, stock){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-    }
+class Ticket {
+  constructor(id, nombre, precio, stock) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+  }
 }
 
-const ticket1 = new Ticket(1, 'Babasonicos', 2900, 300);
-const ticket2 = new Ticket(2, 'El mato', 5000, 100);
-const ticket3 = new Ticket(3, 'Tren Loco', 1500, 400);
-const ticket4 = new Ticket(4, 'Celia Cruz', 1000, 200);
+const ticket1 = new Ticket(1, "Babasonicos", 2900, 300);
+const ticket2 = new Ticket(2, "El mato", 5000, 100);
+const ticket3 = new Ticket(3, "Tren Loco", 1500, 400);
+const ticket4 = new Ticket(4, "Celia Cruz", 1000, 200);
 
 const entradas = [];
-entradas.push(ticket1,ticket2,ticket3,ticket4);
+entradas.push(ticket1, ticket2, ticket3, ticket4);
 
-let opcionEntrada = Number(prompt(
+let opcionEntrada = Number(
+  prompt(
     `1) Babasonicos - $2900
     2) El Mato - $5000
     3) Tren Loco - $1500
     4) Celia Cruz - $1000`
-));
+  )
+);
 
-const ticketComprado = [];
-
-switch(opcionEntrada){
-    case 1:
-    comprar(opcionEntrada);
-    break;
-    case 2:
-    comprar(opcionEntrada);
-    break;
-    case 3:
-    comprar(opcionEntrada);
-    break;
-    case 4:
-    comprar(opcionEntrada);
-    break;
-    default:
-    alert('Opción incorrecta');
-    break;
+if (opcionEntrada) {
+  //El switch no hacia falta, directamente valido que exista opcionEntrada y luego llamo a comprar
+  comprar(opcionEntrada);
 }
 
-function comprar(opcionEntrada){
-    id = opcionEntrada - 1;
-    alert(`Usted ha seleccionado entradas de ${entradas[id].nombre}`);
+function comprar(opcionEntrada) {
+  const ticketComprado = []; //meti el array aca para que quede mas ordenado
 
-    this.stock = Number(prompt(`cantidad de entradas`));
-    ticketComprado.push(new Ticket(entradas[id].id, entradas[id].nombre, entradas[id].precio * stock, stock));
+  const selectedTicket = entradas.find(
+    //Hago un find, para que me devuelva el objeto con los datos de esa entrada
+    (entrada) => entrada.id === opcionEntrada
+  );
+  alert(`Usted ha seleccionado entradas de ${selectedTicket.nombre}`); //Luego uso selectedTicket para mostrar los datos
 
-    alert(`Compro entradas de ${ticketComprado[0].nombre}
+  let cantEntradas = Number(prompt(`cantidad de entradas`));
+  ticketComprado.push(
+    new Ticket(
+      selectedTicket.id,
+      selectedTicket.nombre,
+      selectedTicket.precio * cantEntradas, //Genero el ticket con selectedTicket y luego consologueo
+      cantEntradas
+    )
+  );
+
+  alert(`Compro entradas de ${ticketComprado[0].nombre}
             Cantidad: ${ticketComprado[0].stock}
             Precio Total: $${ticketComprado[0].precio}`);
-    console.log(ticketComprado);
+  console.log(ticketComprado);
 }
